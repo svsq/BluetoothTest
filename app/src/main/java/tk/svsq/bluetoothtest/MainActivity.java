d69769b761772bc332a925876bd2056dc8d65528
Slaventij Bilokur <svsq@outlook.com>
2018-05-12 15:07:03 +0300

    Init Bluetooth

--- app/src/main/java/tk/svsq/bluetoothtest/MainActivity.java
+++ app/src/main/java/tk/svsq/bluetoothtest/MainActivity.java
@@ -1,13 +1,44 @@
 package tk.svsq.bluetoothtest;
 
+import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView labelStateBluetooth;
+    BluetoothAdapter bluetoothAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        labelStateBluetooth = findViewById(R.id.tvBTstate);
+        Button buttonBluetoothState = findViewById(R.id.btnCheck);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        buttonBluetoothState.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bluetoothAdapter == null) {
+                    labelStateBluetooth.setText("Bluetooth isn't supported on your device");
+                } else {
+                    if(bluetoothAdapter.isEnabled()) {
+                        if(bluetoothAdapter.isDiscovering()) {
+                            labelStateBluetooth.setText("Bluetooth is turning on");
+                        } else {
+                            labelStateBluetooth.setText("Bluetooth is ready");
+                        }
+                    } else {
+                        labelStateBluetooth.setText("Bluetooth isn't accessible!");
+                    }
+                }
+            }
+        });
     }
 }
